65b3e7ada9e8dd4e714d79e649a23a57592cecf9
shivforevershakti <shivforevershakti@gmail.com>
2025-05-06 00:49:54 +0530

    boiler plate ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
-// src/App.jsx
-import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello from App.jsx</h1>
-    </div>
-  );
-};
+import React from 'react';
+import ErrorPage from './error//ErrorPage';
+import ErrorBoundary from './error/ErrorBoundary';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const App =()=> {
+    const router = createBrowserRouter([
+        {
+            path: "",
+            element: (
+                <ErrorBoundary>
+                    <div>Hello</div>
+                </ErrorBoundary>
+            ),
+        },
+        {
+            path: "login",
+            element: (
+                <ErrorBoundary>
+                    <div>Hello</div>
+                </ErrorBoundary>
+            ),
+        },
+     
+        {
+            path: "register",
+            element: (
+                <ErrorBoundary>
+                    <div>Hello</div>
+                </ErrorBoundary>
+            ),
+        },
+     
+     
+        {
+            path: "properties",
+            element: (
+                <ErrorBoundary>
+                   <div>Hello</div>
+                </ErrorBoundary>
+            ),
+        },
+       
+        { path: "*", element: <ErrorPage /> },
+    ]);
+
+    return (
+        <RouterProvider router={router} />
+    );
+}
 
 export default App;
